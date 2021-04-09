fdf3fe9de86fd0400da27db821f0d1278482f608
Alex Cyrus <alex@alexcyrus.com>
2021-04-09 15:54:07 -0500

    set up react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Navbar />
     </div>
   );
 }
