35495c38b438ffaf3a3f29b7a603c43a7b7eaef1
Alex Cyrus <alex@alexcyrus.com>
2021-04-28 11:08:51 -0400

    close navbar on mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,7 @@ function Navbar() {
     <div>
       <nav className="navbar">
         <div className="navbar-container">
-          <Link to="/" className="navbar-logo">
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
             Alex Cyrus
           </Link>
           <div className="menu-icon" onClick={handleClick}>
