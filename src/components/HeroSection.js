1cba6323ab264345a7aa1916b0de6325036b311e
Alex Cyrus <alex@alexcyrus.com>
2021-04-28 11:39:28 -0400

    implement hero section

---
+++ src/components/HeroSection.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css';
+import './HeroSection.css';
+
+function HeroSection() {
+  return (
+    <div className='hero-container'>
+        <h1>ALEX CYRUS</h1>
+        <h3>WEB DEVELOPER</h3>
+    </div>
+  )
+}
+
+export default HeroSection
