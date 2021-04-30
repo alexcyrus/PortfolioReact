2e9f420b6c0ffac05ee445dc4e3cd5dd28f949ac
Alex Cyrus <alex@alexcyrus.com>
2021-04-30 15:02:44 -0400

    add portfolio images

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -5,40 +5,40 @@ import CardItem from './CardItem';
 function Cards() {
   return (
     <div className='cards'>
-      <h1>Check out these EPIC Destinations!</h1>
+      <h1>Work Samples</h1>
       <div className='cards__container'>
         <div className='cards__wrapper'>
           <ul className='cards__items'>
             <CardItem
               src='images/cr-edge.png'
-              text='Explore the hidden waterfall deep inside the Amazon Jungle'
-              label='Adventure'
+              text='CR-Edge'
+              label='Freelance'
               path='/services'
             />
             <CardItem
-              src='images/img-2.jpg'
-              text='Travel through the Islands of Bali in a Private Cruise'
-              label='Luxury'
+              src='images/turn_up_the_vamp.jpg'
+              text='Turn Up The Vamp'
+              label='Video Game'
               path='/services'
             />
           </ul>
           <ul className='cards__items'>
             <CardItem
-              src='images/img-3.jpeg'
-              text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
-              label='Mystery'
+              src='images/saks_fifth_avenue.jpg'
+              text='Saks Fifth Avenue'
+              label='Brickwork Software'
               path='/services'
             />
             <CardItem
-              src='images/img-4.jpg'
-              text='Experience Football on Top of the Himilayan Mountains'
-              label='Adventure'
+              src='images/kate_spade.jpg'
+              text='Kate Spade'
+              label='Brickwork Software'
               path='/products'
             />
             <CardItem
-              src='images/img-8.jpg'
-              text='Ride through the Sahara Desert on a guided camel tour'
-              label='Adrenaline'
+              src='images/carolina_herrera.jpg'
+              text='Carolina Herrera'
+              label='Brickwork Software'
               path='/sign-up'
             />
           </ul>
