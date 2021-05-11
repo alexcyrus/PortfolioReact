c65b4f2b22949d8ae9e5af8f096a580cdb7803c9
Alex Cyrus <alex@alexcyrus.com>
2021-05-11 13:50:14 -0400

    add headshot to about page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import './Footer.css';
-import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
     <div className='footer-container'>
       <section className='footer-subscription'>
         <h1>About</h1>
-        <p>Front End Developer with strong design and communication skills. I combine pragmatism and craftsmanship to help my teams create winning software. My work philosophy is kaizen, also known as continuous improvement, without neglecting performance optimization, clean code, and excellent user experience. Always love taking on new challenges and cross-team collaborating.</p>
+        <div className='image-container'>
+          <img src='images/headshot.jpg'></img>
+          <p>Front End Developer with strong design and communication skills. I combine pragmatism and craftsmanship to help my teams create winning software. My work philosophy is kaizen, also known as continuous improvement, without neglecting performance optimization, clean code, and excellent user experience. Always love taking on new challenges and cross-team collaborating.</p>
+          <br />
+          <p>When I'm not coding, you can find me exercising at the gym, enjoying a nice bike ride, singing karaoke, or reading a book at the local hipster cafe.</p>
+          <br />
+          <p>Please feel free to browse my portfolio!</p>
+        </div>
       </section>
     </div>
   );
