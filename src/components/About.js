b899646053c574b4ac53b2ad54c4a961eacf8096
alexcyrus <alex@alexcyrus.com>
2024-06-09 15:51:45 -0500

    update about section

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,8 @@ function About() {
         <h1>About</h1>
         <div className='image-container'>
           <img src='images/headshot.jpg' />
-          <p>Front End Developer with strong design and communication skills. I combine pragmatism and craftsmanship to help my teams create winning software. My work philosophy is kaizen, also known as continuous improvement, without neglecting performance optimization, clean code, and excellent user experience. Always love taking on new challenges and cross-team collaborating.</p>
+          <p>Software Developer with strong design and communication skills. I combine pragmatism and craftsmanship to help my teams create winning software. My work philosophy is kaizen, which involves continuous self-improvement, staying
+          current of industry standards, and taking on new challenges and cross-team collaboration.</p>
           <p>When I'm not coding, you can find me exercising at the gym, enjoying a nice bike ride, singing karaoke, or reading a book at the local cafe.</p>
           <p>Please feel free to browse my portfolio!</p>
         </div>
